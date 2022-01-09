8849904a6670ceebe8b530c1c1121050fdc14338
Tathagata Mondal <tathagata.mandal@zimyo.com>
2022-01-09 21:43:18 +0530

    install Material UI

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import Button from '@mui/material/Button';
 
 const LoginPage=()=>{
    
-    return <div>Welcome to the Login Page</div>
+    return(
+        <>
+            <div>Welcome to the Login Page</div>
+            <p>
+                <Button variant="contained">Button</Button>
+            </p>
+
+        </>
+    ) 
 }
 
 export default LoginPage
