0690efefbbac320941cfac8403a0f9e195061870
Asamanja Kasundi <asamanjakasundi@gmail.com>
2022-01-10 14:55:12 +0530

    start Styling In Login page

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import styles from '../Styles/login.module.css';
+
 
 const LoginPage=()=>{
    
     return(
         <>
+             <div className = {styles.styleLogin}></div>
             <div>Welcome to the Login Page</div>
             <p>
                 <Button variant="contained">Button</Button>
