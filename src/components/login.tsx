669509c302f6c412eb6d4d720fb32267b8099abf
Tathagata Mondal <tathagata.mandal@zimyo.com>
2022-01-11 15:32:34 +0530

    loading in sign in

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
 import Button from '@mui/material/Button';
 import { makeStyles } from '@mui/styles';
 import TextField from '@mui/material/TextField';
 import styles from '../Styles/login.module.css';
 import logo from '../Assets/Images/potatoes-1585060.jpg';
-import { width } from '@mui/system';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const useStyles = makeStyles({
     inputBox: {
@@ -16,11 +16,21 @@ const useStyles = makeStyles({
         background:"linear-gradient(90deg, #6B59BE 0%, #4D7EB7 51%, #3F4A8C 100%)",
         width: "100%",
         textTransform: "none",
+    },
+    signInButtonloading: {
+        background:"linear-gradient(90deg, #6B59BE 0%, #4D7EB7 51%, #3F4A8C 100%)",
+        width: "100%",
+        height: "36px",
+        textTransform: "none",
+        opacity: "0.4",
     }
-  });
+});
 
 const LoginPage=()=>{
+
     const classes = useStyles();
+    const [loading , setloading] = useState(true);
+
     return(
         <>
             <div className= {styles.splitLeft}>
@@ -45,12 +55,21 @@ const LoginPage=()=>{
                     />
                 </div>
                 <div className = {styles.buttoninputboxdiv}>
-                    <Button 
-                        variant="contained" 
-                        className = {classes.signInButton}
-                    >
-                        Sign in
-                    </Button>
+                    {!loading ?
+                        <Button 
+                            variant="contained" 
+                            className = {classes.signInButton}
+                        >
+                            Sign in
+                        </Button>
+                        :
+                        <Button 
+                            variant="contained" 
+                            className = {classes.signInButtonloading}
+                        >
+                            <CircularProgress color="inherit" size={30}/>
+                        </Button>
+                    }
                 </div>
                 <div className = {styles.signuplinkbox}>
                     <p>Don't have an account? <a href = "#">Sign up</a></p>
